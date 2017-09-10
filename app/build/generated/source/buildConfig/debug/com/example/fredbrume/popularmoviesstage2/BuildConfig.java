3f6649134a7d7839cedf13708a055c6c0db3f4cd
FredBrume <FredBrume@users.github.com>
2017-09-09 23:35:36 -0400

    ......

--- app/build/generated/source/buildConfig/debug/com/example/fredbrume/popularmoviesstage2/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/fredbrume/popularmoviesstage2/BuildConfig.java
@@ -11,5 +11,5 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
   // Fields from build type: debug
-  public static final String API_KEY = "3d0e744e4523e7814d41c3aeaee39ccb";
+  public static final String API_KEY = "";
 }
